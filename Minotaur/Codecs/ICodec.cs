2ede8d27c949326a776bdd3698df175c08913784
fdieulle <fabien.dieulle@gmail.com>
2018-07-18 23:02:23 +0200

    Introduce ICodec interface and write a dummy implementation

---
+++ Minotaur/Codecs/ICodec.cs
@@ -0,0 +1,28 @@
+﻿namespace Minotaur.Codecs
+{
+    /// <summary>
+    /// Defines the interface to encode and decode data.
+    /// </summary>
+    public unsafe interface ICodec
+    {
+        /// <summary>
+        /// Encode data from src to dst.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="lSrc"></param>
+        /// <param name="dst"></param>
+        /// <param name="lDst"></param>
+        /// <returns>Number of data written</returns>
+        int Encode(ref byte* src, int lSrc, ref byte* dst, int lDst);
+
+        /// <summary>
+        /// Decode data from src to dst.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="lSrc"></param>
+        /// <param name="dst"></param>
+        /// <param name="lDst"></param>
+        /// <returns>Number of data read</returns>
+        int Decode(ref byte* src, int lSrc, ref byte* dst, int lDst);
+    }
+}
---
+++ Minotaur/Codecs/VoidCodec.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Minotaur.Codecs
+{
+    public unsafe class VoidCodec : ICodec
+    {
+        #region Implementation of ICodec
+
+        public int Encode(ref byte* src, int lSrc, ref byte* dst, int lDst)
+        {
+            var count = Math.Min(lSrc, lDst);
+            Buffer.MemoryCopy(src, dst, count, count);
+            src += count;
+            dst += count;
+            return count;
+        }
+
+        public int Decode(ref byte* src, int lSrc, ref byte* dst, int lDst)
+        {
+            var count = Math.Min(lSrc, lDst);
+            Buffer.MemoryCopy(src, dst, count, count);
+            src += count;
+            dst += count;
+            return count;
+        }
+
+        #endregion
+    }
+}

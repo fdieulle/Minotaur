8822b2792f107a196c40a0aa5defbc6b6a6cf4ef
fdieulle <fabien.dieulle@gmail.com>
2018-07-25 23:16:52 +0200

    Create SizeofOf function for native types

---
+++ Minotaur/Native/Natives.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Minotaur.Native
+{
+    public static class Natives
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int SizeOfEntry<T>()
+        {
+            if (typeof(T) == typeof(int) || typeof(T) == typeof(Int32Entry))
+                return Int32Entry.SIZE;
+            if (typeof(T) == typeof(float) || typeof(T) == typeof(FloatEntry))
+                return FloatEntry.SIZE;
+            if (typeof(T) == typeof(long) || typeof(T) == typeof(Int64Entry))
+                return Int64Entry.SIZE;
+            if (typeof(T) == typeof(double) || typeof(T) == typeof(DoubleEntry))
+                return DoubleEntry.SIZE;
+            if (typeof(T) == typeof(string) || typeof(T) == typeof(StringEntry))
+                return StringEntry.SIZE;
+
+            throw new NotSupportedException($"This data type: {typeof(T)} isn't supported");
+        }
+    }
+}
